71d3a16e0768f3a5d18d186deac8081fac6fb057
Tacianojr07 <t.juniior13@gmail.com>
2023-09-07 12:50:22 -0300

    start project with dependencies

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NativeBaseProvider,Text,Center, Box,Button } from 'native-base';
+import { useState } from 'react';
+import { THEME } from './src/styles/theme';
+import {useFonts, Roboto_400Regular,Roboto_500Medium, Roboto_700Bold} from "@expo-google-fonts/roboto"
+
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular,Roboto_500Medium,Roboto_700Bold})
+
+
+
+  const [contador,setContador] = useState(1);
+
+  function handleOneMore(){
+     setContador(valorAtual => valorAtual + 1);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1}>
+        
+        <Text fontFamily="body">Hello Word!</Text>
+        <Text>{contador}</Text>
+        <Button bgColor='cyan.500' width={100} onPress={handleOneMore}>MAIS UM</Button>
+
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from 'native-base';
+
+export const THEME = extendTheme({
+    colors: {
+        cyan: {
+            500: '#9DD9E7'
+        }
+    },
+
+    fonts: {
+        heading: 'Roboto_700Bold',
+        body: 'Roboto_400Regular',
+        medium: 'Roboto_500Medium',
+      },
+})
