cef17f9e486829e03be174d4f1b78a13969dc250
Tacianojr07 <t.juniior13@gmail.com>
2023-09-18 09:45:08 -0300

    feat: add localStorage

--- src/context/utils.ts
+++ src/context/utils.ts
@@ -1,5 +1,22 @@
+import { UserDataProps } from "../../@types/types";
 import { api } from "../services/api";
 
+
+export function serUserLocalStorage(user:UserDataProps | null) {
+    localStorage.setItem('u', JSON.stringify(user));
+}
+
+export function getUserLocalStorage () {
+    const json = localStorage.getItem('u');
+
+    if(!json) {
+        return null
+    }
+
+    const user = JSON.stringify(json)
+
+    return user ?? null
+}
 
 export async function  LoginRequest(email:string, password: string) {
     try {
