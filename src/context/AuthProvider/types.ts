7639612b6f42c4caaea21aed47010a568b4ad3c6
Tacianojr07 <t.juniior13@gmail.com>
2023-12-04 12:31:08 -0300

    refatoração de types

--- src/context/AuthProvider/types.ts
+++ src/context/AuthProvider/types.ts
@@ -3,15 +3,11 @@ export interface IUser {
   password?: string;
   email?: string;
   token?: string;
-  name?: string;
 }
 
 export interface IAuthProps extends IUser {
   user: IUser | null;
-  LoginToken: (email: string, password: string,username:string) => Promise<boolean>;
-  authenticate: (email: string, password: string,username:string) => Promise<void>;
-  logout: () => void;
-  signed: boolean;
+  LoginToken: (email: string, password: string) => Promise<boolean>;
 }
 
 export interface IAuthProviderProps {
